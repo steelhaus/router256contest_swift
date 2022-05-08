a5a11b8abdcb7ceb4fa9d120a18be085eb32517d
Точилин Анатолий Витальевич <ATochilin@inno.tech>
2022-05-08 04:08:21 +0300

    E2 ready

--- sandbox/E2.swift
+++ sandbox/E2.swift
@@ -1,3 +1,21 @@
+extension Array {
+    func insertionIndexOf(_ elem: Element, isOrderedBefore: (Element, Element) -> Bool) -> Int {
+        var lo = 0
+        var hi = self.count - 1
+        while lo <= hi {
+            let mid = (lo + hi)/2
+            if isOrderedBefore(self[mid], elem) {
+                lo = mid + 1
+            } else if isOrderedBefore(elem, self[mid]) {
+                hi = mid - 1
+            } else {
+                return mid // found at position mid
+            }
+        }
+        return lo // not found, would be inserted at position lo
+    }
+}
+
 struct Task {
     let start: Int
     let duration: Int
@@ -12,7 +30,7 @@ class Queue {
     private let threadsLimit: Int
 
     /// Minimum duration of all added tasks
-    private var minDuration: Int = 0
+//    private var minDuration: Int = 0
     /// Duration remains for different tasks (tasks order does not matter)
     private var threadsRemains: [Int] = []
 
@@ -23,28 +41,74 @@ class Queue {
     }
 
     func waitUntilReadyToAcceptTaskAt(_ start: Int) {
-        if threadsRemains.count >= threadsLimit {
-            wait(minDuration)
+        let timeBeforeStart = max(start - currentTime, 0)
+        let timeBeforeQueueGetsAvailable = threadsRemains.count >= threadsLimit ? threadsRemains[0] : 0
+        let timeToWait = max(timeBeforeStart, timeBeforeQueueGetsAvailable)
+        if timeToWait > 0 {
+            wait(timeToWait)
         }
-        if start > currentTime {
-            wait(start - currentTime)
-        }
+//        let timeToWait = max(start - currentTime, 0)
+//        if threadsRemains.count >= threadsLimit {
+//            wait(threadsRemains[0])
+//        }
+//        if start > currentTime {
+//            wait(start - currentTime)
+//        }
     }
 
     func addTask(duration: Int) {
-        minDuration = minDuration == 0 ? duration : min(minDuration, duration)
-        threadsRemains.append(duration)
+        let index = threadsRemains.insertionIndexOf(duration) { $0 < $1 }
+        threadsRemains.insert(duration, at: index)
+//        minDuration = minDuration == 0 ? duration : min(minDuration, duration)
+////        minDuration = min(minDuration, duration)
+//        threadsRemains.append(duration)
     }
 
     private func wait(_ duration: Int) {
+//        print("------")
+//        print("wait for: \(duration)")
+//        print(threadsRemains)
         currentTime += duration
         threadsRemains = threadsRemains.map { $0 - duration }
         threadsRemains.removeAll { $0 <= 0 }
-        minDuration = threadsRemains.min() ?? 0
+//        minDuration = threadsRemains.min() ?? 0
+//        print("min duration remains: \(minDuration)")
+//        print(threadsRemains)
     }
 }
 
+class Data {
+
+    static var currentIndex: Int = 0
+    let queueInfo = QueueInfo(threads: 1, tasksCount: 5)
+    let tasks: [Task] = [
+        // Set 1
+//        .init(start: 1, duration: 10),
+//        .init(start: 2, duration: 9),
+//        .init(start: 3, duration: 8),
+//        .init(start: 4, duration: 7),
+//        .init(start: 5, duration: 6)
+
+        // Set 2
+        .init(start: 1, duration: 10),
+        .init(start: 2, duration: 9),
+        .init(start: 3, duration: 8),
+        .init(start: 4, duration: 7),
+        .init(start: 5, duration: 6)
+    ]
+
+    func getNextTask() -> Task {
+        let task = tasks[Data.currentIndex]
+        Data.currentIndex += 1
+        return task
+    }
+
+}
+
+let data = Data()
+
 func readInt() -> Int {
+//    return 1
     Int(readLine()!)!
 }
 
@@ -53,11 +117,13 @@ func readEmpty() {
 }
 
 func readQueueInfo() -> QueueInfo {
+//    data.queueInfo
     let rawLine = readLine()!.split(separator: " ")
     return QueueInfo(threads: Int(rawLine[0])!, tasksCount: Int(rawLine[1])!)
 }
 
 func readNextTask() -> Task {
+//    data.getNextTask()
     let rawLine = readLine()!.split(separator: " ")
     return Task(start: Int(rawLine[0])!, duration: Int(rawLine[1])!)
 }
